1e4ada6f63b4703cd404fb60dbf1213ba1c51067
Joseph-Joy-Ender <144816729+Joseph-Joy-Ender@users.noreply.github.com>
2024-02-29 06:16:16 +0100

    "dom manipulation"

---
+++ dom/dom.js
@@ -0,0 +1,25 @@
+let banner = document.getElementById('page-banner');
+let page = document.getElementsByClassName('title')
+let ul = document.getElementsByTagName('ul')
+
+let query = document.querySelector('#book-list')
+
+let allQuery = document.querySelectorAll('#book-list li .name')
+
+Array.from(allQuery).forEach((element) => console.log(element.textContent += ('(book title)')))
+
+// console.log(query);
+
+// console.log(ul);
+
+
+// Array.from(page).forEach((element) => console.log(element.innerHTML));
+
+
+// for (let index = 0; index < page.length; index++) {
+//     const element = page[index];
+//     console.log(element);
+//
+// }
+
+// console.log(page[0]);
