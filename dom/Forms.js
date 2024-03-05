258ded8b53b4bae727e48513ddf2377bd4e24967
Joseph-Joy-Ender <144816729+Joseph-Joy-Ender@users.noreply.github.com>
2024-03-05 14:54:37 +0100

    changes

--- dom/Forms.js
+++ dom/Forms.js
@@ -1,11 +1,14 @@
 const form = document.forms['info']
-form.addEventListener('submit', (e)=>{
+
+form.addEventListener('submit', (e)=> {
     e.preventDefault();
 
     const firstName = form.querySelector('#firstname').value;
     const lastName = form.querySelector('#lastname').value;
     const email = form.querySelector('#email').value;
     const password = form.querySelector('#password').value;
+
+    console.log(firstName)
 
     // const obj = {
     //     firstName: "Joy",
@@ -20,13 +23,13 @@ form.addEventListener('submit', (e)=>{
         password: password
     }
 
-    // console.log(obj)
+    console.log(obj)
 
 
-
-    const sendData = async (data) =>{
+    const sendData = async (data) => {
+        console.log(obj)
         try {
-            const response = await fetch("http://localhost:8000/api/user/register",{
+            const response = await fetch("http://localhost:3000/api/user/register", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
@@ -34,12 +37,15 @@ form.addEventListener('submit', (e)=>{
                 body: JSON.stringify(data),
             });
 
+            console.log("in promise", response)
+
             const result = await response.json();
-            console.log("Success:", result.message);
-        }catch (error){
+            console.log("Success:", result);
+        } catch (error) {
             console.error("Error:", error);
         }
     };
 
     sendData(obj);
+
 });
